a481cfdc4749621df60554cb1cad3132533dc688
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-09-06 02:42:17 +0700

    5.7 create organizer

--- src/main/java/se331/lab/controller/OrganizerController.java
+++ src/main/java/se331/lab/controller/OrganizerController.java
@@ -8,35 +8,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import se331.lab.entity.Event;
-import se331.lab.service.EventService;
+import se331.lab.entity.Organizer;
+import se331.lab.service.OrganizerService;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class EventController {
+public class OrganizerController {
 
-    final EventService eventService;
+    final OrganizerService organizerService;
 
-    @GetMapping("events")
-    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage,
-                                           @RequestParam(value = "_page", required = false) Integer page) {
-        List<Event> output = null;
-        Integer eventSize = eventService.getEventSize();
+    @GetMapping("organizers")
+    public ResponseEntity<?> getOrganizerLists(@RequestParam(value = "_limit", required = false) Integer perPage,
+                                               @RequestParam(value = "_page", required = false) Integer page) {
+        List<Organizer> output = null;
+        Integer organizerSize = organizerService.getOrganizerSize();
         HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("x-total-count", String.valueOf(eventSize));
+        responseHeader.set("x-total-count", String.valueOf(organizerSize));
         try {
-            output = eventService.getEvents(perPage, page);
+            output = organizerService.getOrganizers(perPage, page);
             return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
         } catch (IndexOutOfBoundsException ex) {
             return new ResponseEntity<>(output, responseHeader, HttpStatus.NOT_FOUND);
         }
     }
 
-    @GetMapping("events/{id}")
-    public ResponseEntity<?> getEvent(@PathVariable("id") Long id) {
-        Event output = eventService.getEvent(id);
+    @GetMapping("organizers/{id}")
+    public ResponseEntity<?> getOrganizer(@PathVariable("id") Long id) {
+        Organizer output = organizerService.getOrganizer(id);
         if (output != null) {
             return ResponseEntity.ok(output);
         } else {
---
+++ src/main/java/se331/lab/dao/OrganizerDaoImpl.java
@@ -0,0 +1,67 @@
+package se331.lab.dao;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab.entity.Organizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class OrganizerDaoImpl implements OrganizerDao {
+    List<Organizer> organizerList;
+
+    @PostConstruct
+    public void init() {
+        organizerList = new ArrayList<>();
+
+        organizerList.add(Organizer.builder()
+                .id(1L)
+                .organizationName("Kat Laydee")
+                .address("Meow Town")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(2L)
+                .organizationName("Fern Pollin")
+                .address("Floral City")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(3L)
+                .organizationName("Carey Wales")
+                .address("Playa Del Carmen")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(4L)
+                .organizationName("Dawg Dahd")
+                .address("Woof Town")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(5L)
+                .organizationName("Kahn Opiner")
+                .address("Tin City")
+                .build());
+        organizerList.add(Organizer.builder()
+                .id(6L)
+                .organizationName("Brody Kill")
+                .address("Highway 50")
+                .build());
+    }
+
+    @Override
+    public Integer getOrganizerSize() {
+        return organizerList.size();
+    }
+
+    @Override
+    public List<Organizer> getOrganizers(Integer pageSize, Integer page) {
+        pageSize = pageSize == null ? organizerList.size() : pageSize;
+        page = page == null ? 1 : page;
+        int firstIndex = (page - 1) * pageSize;
+        return organizerList.subList(firstIndex, Math.min(firstIndex + pageSize, organizerList.size()));
+    }
+
+    @Override
+    public Organizer getOrganizer(Long id) {
+        return organizerList.stream().filter(organizer -> organizer.getId().equals(id)).findFirst().orElse(null);
+    }
+}
