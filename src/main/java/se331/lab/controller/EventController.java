43d8103c8fcdd3ddbce34b8ae99d8c3b7f23765c
linnlatt <linn.latt.yamone.qz@gmail.com>
2026-09-07 15:01:05 +0700

    (lab8) 5.4 add POST to event

--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -5,10 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se331.lab.entity.Event;
 import se331.lab.service.EventService;
 
@@ -40,5 +37,11 @@ public class EventController {
         } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
+    }
+
+    @PostMapping("/events")
+    public ResponseEntity<?> addEvent(@RequestBody Event event) {
+        Event output = eventService.save(event);
+        return ResponseEntity.ok(output);
     }
 }
